17446181725822cbeb5c9f3cd1e99d399018d97c
hardboil <hardboil@gmail.com>
2020-10-20 01:11:58 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+// ! props.video 를 video로 사용이 가능하다
+// ? const VideoItem = (props) => ();
+// ? const VideoItem = ({video}) => ();
+// ? const VideoItem = ({video: videoItem}) => ();
+// ? const VideoItem = ({video: {snippet}}) => ();
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.video}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
+
+// const VideoItem = (props) => (
+//   <li>
+//     <img src={props.video.snippet.thumbnails.medium.url} alt="thumbnail" />
+//     <div>
+//       <p>{props.video.snippet.title}</p>
+//       <p>{props.video.snippet.channelTitle}</p>
+//     </div>
+//   </li>
+// );
 
 export default VideoItem;
